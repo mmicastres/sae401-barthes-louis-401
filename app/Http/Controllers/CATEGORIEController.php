dfd8feb80aea7398a6dc826c855a9dba99926334
Lyrastico <louisbarthes1@gmail.com>
2023-04-05 12:17:11 +0200

    projects controller finished

--- app/Http/Controllers/CATEGORIEController.php
+++ app/Http/Controllers/CATEGORIEController.php
@@ -2,9 +2,108 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CATEGORIE;
 use Illuminate\Http\Request;
+use Validator;
 
 class CATEGORIEController extends Controller
 {
-    //
+    // Select the details of all categorie
+    public function listCATE(Request $request)
+    {
+        $returned = CATEGORIE::select("ID_CATE ", "ID_USER_CREATED ", "NAME_CATE")->get();
+        return response()->json($returned);
+    }
+    // Select the details of one categorie
+    public function oneCATE($ID_CATE)
+    {
+        $returned = CATEGORIE::select("ID_CATE ", "ID_USER_CREATED ", "NAME_CATE")->where("ID_CATE", "=", $ID_CATE)->get();
+        return response()->json($returned);
+    }
+    // create a new categorie
+    public function newCATE(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'ID_CATE ' => ['required', 'integer'],
+            'ID_USER_CREATED' => ['required', 'integer'],
+            'NAME_CATE' => ['required', 'alpha_num']
+        ]);
+        if ($validator->fails()) {
+            return $validator->error();
+        }
+
+
+        $categorie = new CATEGORIE;
+
+        $categorie->ID_CATE = $request->ID_CATE;
+        $categorie->ID_USER_CREATED = $request->ID_USER_CREATED;
+        $categorie->NAME_CATE = $request->NAME_CATE;
+
+        $ok = $categorie->save();
+
+        if ($ok) {
+            return response()->json(["status" => 1, "message" => "categorie created"], 201);
+        } else {
+            return response()->json([
+                "status" => 0,
+                "message" => "Problems While creating"
+            ], 400);
+        }
+    }
+    // modify a categorie
+    public function modifCATE(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'ID_CATE ' => ['required', 'integer'],
+            'ID_USER_CREATED' => ['required', 'integer'],
+            'NAME_CATE' => ['required', 'alpha_num']
+        ]);
+        if ($validator->fails()) {
+            return $validator->error();
+        }
+
+
+        $categorie = CATEGORIE::where("ID_CATE", "=", $request->ID_CATE)->first();
+
+        if ($categorie) {
+
+            $categorie->ID_CATE = $request->ID_CATE;
+            $categorie->ID_USER_CREATED = $request->ID_USER_CREATED;
+            $categorie->NAME_CATE = $request->NAME_CATE;
+
+
+            $ok = $categorie->save();
+
+            if ($ok) {
+                return response()->json(["status" => 1, "message" => "categorie modified"], 201);
+            } else {
+                return response()->json([
+                    "status" => 0,
+                    "message" => "Problems While Modifying"
+                ], 400);
+            }
+        } else {
+            return response()->json([
+                "status" => 0,
+                "message" => "This categorie doesn't exist !"
+            ], 400);
+        }
+    }
+    // delete an old categorie
+    public function supCATE($ID_CATE)
+    {
+        $categorie = CATEGORIE::where("ID_CATE", "=", $ID_CATE);
+
+        $ok = $categorie->delete();
+
+        if ($ok) {
+            return response()->json(["status" => 1, "message" => "categorie deleted"], 201);
+        } else {
+            return response()->json([
+                "status" => 0,
+                "message" => "This categorie doesn't exist !"
+            ], 400);
+        }
+    }
 }
--- app/Http/Controllers/CrossController.php
+++ app/Http/Controllers/CrossController.php
@@ -15,8 +15,10 @@ class CrossController extends Controller
     {
         $user = USER::select("PSEUDO_USER", "LEVEL_USER", "FORMATION_STUDENT", "YEAR_STUDENT", "SCHOOL_PROF", "TEACHING_PROF")->where("ID_USER", "=", $ID_USER)->first();
         $projects = PROJECT::select("ID_USER_IS_POSTED", "NAME_PROJ", "DESCRIPTION_PROJ", "DATE_PROJ", "IMG_PROJ")->where("ID_USER_IS_POSTED", "=", $ID_USER)->get();
-        $user ["projects"] = $projects;
+        $user["projects"] = $projects;
 
         return response()->json($user);
     }
 }
+
+//SELECT * FROM `PROJECT_CATE` inner join `CATEGORIE` on (PROJECT_CATE.ID_CATE=CATEGORIE.ID_CATE)
