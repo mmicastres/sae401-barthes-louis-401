7ea680b84d013406dd71b4b36c8fe6bd6b2fc67d
Lyrastico <louisbarthes1@gmail.com>
2023-04-14 11:57:19 +0200

    g

--- app/Http/Controllers/CrossController.php
+++ app/Http/Controllers/CrossController.php
@@ -13,7 +13,7 @@ class CrossController extends Controller
 
     public function crossINFO($ID_USER)
     {
-        $user = USER::select("PSEUDO_USER", "LEVEL_USER", "FORMATION_STUDENT", "YEAR_STUDENT", "SCHOOL_PROF", "TEACHING_PROF")->where("ID_USER", "=", $ID_USER)->first();
+        $user = USER::select("PSEUDO_USER", "LEVEL_USER", "FORMATION_STUDENT", "BIO_USER", "NAME_USER", "SURNAME_USER")->where("ID_USER", "=", $ID_USER)->first();
         $projects = PROJECT::select("ID_USER_IS_POSTED", "NAME_PROJ", "DESCRIPTION_PROJ", "DATE_PROJ", "IMG_PROJ")->where("ID_USER_IS_POSTED", "=", $ID_USER)->get();
         $user["projects"] = $projects;
 
